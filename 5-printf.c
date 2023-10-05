540ebf28dbe44637f3679610891bf9d4a916bff6
root <eemmanuel781@gmail.com>
2023-10-05 16:04:27 -0700

    5-printf.c

---
+++ 5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+* main - A program that print a line using the printf funnction 
+* Return: 0 when done
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
