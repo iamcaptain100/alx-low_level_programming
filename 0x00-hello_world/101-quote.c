f1cca5436a6c1e043eca67ce570ad41fba9d0a51
root <eemmanuel781@gmail.com>
2023-10-06 08:09:34 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,2 +1,12 @@
-#!/bin/bash
-gcc -S -masm=intel $xCFILE
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A c program that prints a line to the stardard error
+ * Return 1 (success)
+ */
+
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",stdout);
+return(1);
+}
