70eba158b4c741e862bf91be69eeb210a3cc9e36
root <eemmanuel781@gmail.com>
2023-10-16 09:10:23 -0700

    4-print_rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,18 +6,17 @@
  */
 void print_rev(char *s)
 {
-	int longi = 0;
-	int o;
-
-	while (*s != '\0')
-	{
-		longi++;
-		s++;
-	}
-	s--;
-	for (o = longi; 0 > 0; o--)
-	{
-		_putchar(*s);
+int longi = 0;
+int o;
+while (*s != '\0')
+{
+longi++;
+s++;
+}
+s--;
+for (o = longi; o > 0; o--)
+{
+_putchar(*s);
 s--;
 }
 _putchar('\n');
